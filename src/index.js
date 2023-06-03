a5b36a9b8608591c5cc89b5e85a495e067fe62cf
sambhaji dhore <sambhajisd4@gmail.com>
2023-06-03 22:06:32 +0530

    about me section added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+// const Typed = require("typed.js")
+
 let hamburger=document.querySelector(".hamburger");
 hamburger.onclick= function(){
     let navlist=document.querySelector(".nav-list");
     navlist.classList.toggle("active");
 }
+
+// var typed = new Typed(".typing", {
+//     strings: ["Node.js Backend developer", "Full Stack-Web developer"],
+//     loop: true,
+//     typeSpeed: 50,
+//     backSpeed: 25,
+//     backDelay: 500,
+// });
