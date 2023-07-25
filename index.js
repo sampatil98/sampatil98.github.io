03c4cb0cb6978853414ca8ed1b1cf4fadedebb48
sambhaji dhore <sambhajisd4@gmail.com>
2023-07-25 17:01:22 +0530

    changes

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ window.onscroll = () => {
 
 GitHubCalendar(".calendar", "sampatil98", { responsive: true });
 
-var typed = new Typed("#typing", {
-  strings: ["Full Stack-Web developer"],
-  loop: true,
-  typeSpeed: 60,
-  backSpeed: 45,
-  backDelay: 400,
-});
+// var typed = new Typed("#typing", {
+//   strings: ["Full Stack-Web developer"],
+//   loop: true,
+//   typeSpeed: 60,
+//   backSpeed: 45,
+//   backDelay: 400,
+// });
 
 // document.getElementById('resume-button-1').addEventListener("click", () => {
 //   window.open("https://drive.google.com/file/d/1UAj5f-fHiInGHRnJJjCvNHGGwzpm3RMo/view?usp=share_link", "_blank");
